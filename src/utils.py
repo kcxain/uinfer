0fcab8077b48d83cc88c8b7303196fd13af0434b
kcxain <kcxain@gmail.com>
2025-07-08 19:21:57 +0800

    fix: gpu group set

--- src/utils.py
+++ src/utils.py
@@ -1,18 +1,21 @@
 import os
-
+from loguru import logger
 
 def get_gpu_groups(model_size: int):
-    if model_size == 7 or model_size == 8:
-        # get gpu list
-        gpu_list = os.environ.get("CUDA_VISIBLE_DEVICES", "").split(",")
-        gpu_groups = []
+    logger.info(f"Using model size: {model_size}")
+    gpu_list = os.environ.get("CUDA_VISIBLE_DEVICES", "").split(",")
+    gpu_groups = []
+    if model_size == 4:
+        for gi in gpu_list:
+            gpu_groups.append([gi])
+    elif model_size == 7 or model_size == 8:
         for i in range(0, len(gpu_list), 2):
             if i + 1 < len(gpu_list):
                 gpu_groups.append([gpu_list[i], gpu_list[i + 1]])
             else:
                 gpu_groups.append([gpu_list[i]])
-        return gpu_groups
     else:
         raise ValueError(
-            "Unsupported model size. Please provide a model size of 7 or 8."
+            "Unsupported model size."
         )
+    return gpu_groups
